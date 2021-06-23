938552cc6ab1d03f8c5559977f9eb3c7bccd5cd0
Patricia Faustino de Brito de Jesus <patricia.faustino@iteris.com.br>
2021-06-23 20:41:40 -0300

    Atualizando classes

--- ByteBankInterfaceHeranca/GerenciadorBonificacao.cs
+++ ByteBankInterfaceHeranca/GerenciadorBonificacao.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ByteBank.Funcionarios;
+using ByteBankInterfaceHeranca.Funcionarios;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,21 @@ namespace ByteBankInterfaceHeranca
 {
     class GerenciadorBonificacao
     {
+        private double _totalBonificacao;
+        public void Registrar(Funcionario funcionario)
+        {
+            _totalBonificacao += funcionario.GetBonificacao();
+        }
+
+        //Método com sobrecargas diferentes
+        public void Registrar(Diretor diretor)
+        {
+            _totalBonificacao += diretor.GetBonificacao();
+        }
+
+        public double GetTotalBonificacao()
+        {
+            return _totalBonificacao;
+        }
     }
 }
