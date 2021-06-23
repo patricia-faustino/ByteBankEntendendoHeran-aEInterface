414139de2dee5b5a12d28aa84f60be47d9ec671f
Patricia Faustino de Brito de Jesus <patricia.faustino@iteris.com.br>
2021-06-23 20:23:45 -0300

    Atualizando organizacao dos arquivos

---
+++ ByteBankInterfaceHeranca/Funcionarios/Funcionario.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+    public class Funcionario
+    {
+        //0 - funcionário
+        //1 - diretor
+        //2 - designer
+        //3 - gerente de conta corrente
+        //4- Coordenador
+        // N - ...
+
+        private int _tipo;
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+
+        public double Salario { get; set; }
+
+        public Funcionario(int tipo)
+        {
+            _tipo = tipo;
+        }
+        public double GetBonificacao()
+        {
+            if(_tipo == 1)
+            {
+                return Salario;
+            }
+            return Salario * 0.10;
+        }
+    }
+}
---
+++ ByteBankInterfaceHeranca/Program.cs
@@ -0,0 +1,25 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankInterfaceHeranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario carlos = new Funcionario(1);
+            carlos.Nome = "Carlos Almeida";
+            carlos.CPF = "123-456-789-10";
+            carlos.Salario = 2000;
+
+            Console.WriteLine(carlos.Nome);
+            Console.WriteLine(carlos.GetBonificacao());
+
+            Console.ReadLine();
+        }
+    }
+}
